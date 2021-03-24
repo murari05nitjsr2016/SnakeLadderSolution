bdb8aead65a46e8fefeb87e098f88c2986d6e0ba
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 23:25:22 +0530

    ensured winnig position

--- src/com/snakeladder/SnakeLadderSolution.java
+++ src/com/snakeladder/SnakeLadderSolution.java
@@ -3,38 +3,42 @@ package com.snakeladder;
 import java.util.Random;
 
 public class SnakeLadderSolution {
-	
+
 	public static void main(String[] args){
 		System.out.println("Welcome in snake ladder game");
 		final int WINNING_POSITION = 100;
 		int position = 0;
-		//this fun is used for getting random no.
+		System.out.println("initial position "+position);
 		while(position <= WINNING_POSITION)
 		{
-		Random rand = new Random();
-		int diceValue = rand.nextInt(6)+1;
-		System.out.println(diceValue);
-		int option=rand.nextInt(3);
-		if(option == 0)
-		{
-			System.out.println("no play so your position is same as previous ");
+			//this fun is used for getting random no.
+			Random rand = new Random();
+			int diceValue = rand.nextInt(6)+1;
+			System.out.println("dice value"+diceValue);
+			int option=rand.nextInt(3);
+			System.out.println("option is "+option);
+			if(option == 0)
+			{
+				System.out.println("no play so your position is same as previous ");
 
-		}
-		else if(option == 1)
-		{
-			System.out.println("you got a ladder,your position will increment by dice value ");
-			position = position + diceValue;
-			System.out.println(position);
-		}
-		else
-		{
-			System.out.println("you got a snake,your position will decrement by dice value ");
-			position = position - diceValue;
-			System.out.println(position);
-			if (position < 0)
-				position = 0;
+			}
+			else if(option == 1)
+			{
+				System.out.println("you got a ladder,your position will increment by dice value ");
+				if(position + diceValue <= WINNING_POSITION)
+					position = position + diceValue;
 
-		}
+				System.out.println(position);
+			}
+			else
+			{
+				System.out.println("you got a snake,your position will decrement by dice value ");
+				position = position - diceValue;
+				System.out.println(position);
+				if (position < 0)
+					position = 0;
+
+			}
 		}
 
 	}
