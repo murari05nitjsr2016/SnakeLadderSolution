79cdbf7857f6ecf023b676e47e502f78fce3ec45
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 22:39:18 +0530

    checked for option

--- src/com/snakeladder/SnakeLadderSolution.java
+++ src/com/snakeladder/SnakeLadderSolution.java
@@ -10,6 +10,26 @@ public class SnakeLadderSolution {
 		Random rand = new Random();
 		int diceValue = rand.nextInt(6)+1;
 		System.out.println(diceValue);
+		int option=rand.nextInt(3);
+		if(option == 0)
+		{
+			System.out.println("no play so your position is same as previous ");
+
+		}
+		else if(option == 1)
+		{
+			System.out.println("you got a ladder,your position will increment by dice value ");
+			position = position + diceValue;
+			System.out.println(position);
+		}
+		else
+		{
+			System.out.println("you got a snake,your position will decrement by dice value ");
+			position = position - diceValue;
+			System.out.println(position);
+
+		}
+
 	}
 
 }
