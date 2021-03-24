d8330173052d5d4b9909c9595ee75573bd7bbd06
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 23:44:05 +0530

    reporting the position as well as rollcount

--- src/com/snakeladder/SnakeLadderSolution.java
+++ src/com/snakeladder/SnakeLadderSolution.java
@@ -7,6 +7,7 @@ public class SnakeLadderSolution {
 	public static void main(String[] args){
 		System.out.println("Welcome in snake ladder game");
 		final int WINNING_POSITION = 100;
+		int diceRollCount = 0;
 		int position = 0;
 		System.out.println("initial position "+position);
 		while(position <= WINNING_POSITION)
@@ -19,27 +20,42 @@ public class SnakeLadderSolution {
 			System.out.println("option is "+option);
 			if(option == 0)
 			{
-				System.out.println("no play so your position is same as previous ");
-
+				//System.out.println("no play so your position is same as previous ");
+				System.out.println("position  "+position+" , "+"diceRollCount  "+diceRollCount);
 			}
 			else if(option == 1)
 			{
-				System.out.println("you got a ladder,your position will increment by dice value ");
+				diceRollCount++;
+				//System.out.println("you got a ladder,your position will increment by dice value ");
 				if(position + diceValue <= WINNING_POSITION)
+				{
 					position = position + diceValue;
+					System.out.println("position  "+position+" , "+"diceRollCount  "+diceRollCount);
 
-				System.out.println(position);
+				}
+
+				//System.out.println(position);
+				else
+					System.out.println("position  "+position+" , "+"diceRollCount  "+diceRollCount);
+
 			}
 			else
+			{   diceRollCount++;
+			//System.out.println("you got a snake,your position will decrement by dice value ");
+			position = position - diceValue;
+			System.out.println("position  "+position+" , "+"diceRollCount  "+diceRollCount);
+
+			System.out.println(position);
+			if (position < 0)
 			{
-				System.out.println("you got a snake,your position will decrement by dice value ");
-				position = position - diceValue;
-				System.out.println(position);
-				if (position < 0)
-					position = 0;
+				position = 0;
+				System.out.println("position  "+position+" , "+"diceRollCount  "+diceRollCount);
+
+			}
 
 			}
 		}
+		System.out.println(diceRollCount);
 
 	}
 
