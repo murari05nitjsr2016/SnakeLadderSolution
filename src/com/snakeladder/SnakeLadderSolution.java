527271f79a00a209019a27c0b3e7734d62c933fa
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 22:19:49 +0530

    generate random no for dice

--- src/com/snakeladder/SnakeLadderSolution.java
+++ src/com/snakeladder/SnakeLadderSolution.java
@@ -1,9 +1,15 @@
 package com.snakeladder;
 
+import java.util.Random;
+
 public class SnakeLadderSolution {
 	public static void main(String[] args){
 		System.out.println("Welcome in snake ladder game");
 		int position = 0;
+		//this fun is used for getting random no.
+		Random rand = new Random();
+		int diceValue = rand.nextInt(6)+1;
+		System.out.println(diceValue);
 	}
 
 }
