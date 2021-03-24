52bdd1fb8ac50c0347ba46ff5d5008d90a88b313
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-03-24 22:51:55 +0530

    repeated till winning position

--- src/com/snakeladder/SnakeLadderSolution.java
+++ src/com/snakeladder/SnakeLadderSolution.java
@@ -3,10 +3,14 @@ package com.snakeladder;
 import java.util.Random;
 
 public class SnakeLadderSolution {
+	
 	public static void main(String[] args){
 		System.out.println("Welcome in snake ladder game");
+		final int WINNING_POSITION = 100;
 		int position = 0;
 		//this fun is used for getting random no.
+		while(position <= WINNING_POSITION)
+		{
 		Random rand = new Random();
 		int diceValue = rand.nextInt(6)+1;
 		System.out.println(diceValue);
@@ -27,7 +31,10 @@ public class SnakeLadderSolution {
 			System.out.println("you got a snake,your position will decrement by dice value ");
 			position = position - diceValue;
 			System.out.println(position);
+			if (position < 0)
+				position = 0;
 
+		}
 		}
 
 	}
